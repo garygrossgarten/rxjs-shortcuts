234d02f8023f5de1f287bcc8ac4851df3e99d349
garygrossgarten <gary@fivethree.io>
2020-06-06 00:12:42 +0200

    add codes enum

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { fromEvent, of, merge, combineLatest, Observable } from "rxjs";
 import {
   switchMap,
   distinctUntilChanged,
   share,
   filter,
-  tap,
   map,
 } from "rxjs/operators";
+import { KeyCode } from "./keycodes";
 
 @Component({
   selector: "app-root",
@@ -19,34 +19,84 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     // keyup not getting triggered when meta key is pressed on mac
-    const cmdJ = createShortcut("Meta+j");
-
-    const ctrlL = createShortcut("Control+l");
-
-    const abc = createShortcut("a+b+c");
-
-    this.shortcuts$ = merge(cmdJ, ctrlL, abc).pipe(
-      map((arr) => arr.map((a) => a.key).join("+"))
+    const cmdJ = merge(
+      createShortcut([KeyCode.MetaRight, KeyCode.KeyJ]),
+      createShortcut([KeyCode.MetaLeft, KeyCode.KeyJ])
     );
+
+    const ctrlL = merge(
+      createShortcut([KeyCode.ControlLeft, KeyCode.KeyL]),
+      createShortcut([KeyCode.ControlLeft, KeyCode.KeyL])
+    );
+
+    const abc = createShortcut([KeyCode.KeyA, KeyCode.KeyB, KeyCode.KeyC]).pipe(
+      sequence()
+    );
+    const qwertz = createShortcut([
+      KeyCode.KeyQ,
+      KeyCode.KeyW,
+      KeyCode.KeyE,
+      KeyCode.KeyR,
+      KeyCode.KeyT,
+      KeyCode.KeyY,
+    ]).pipe(sequence());
+
+    const commaDot = createShortcut([KeyCode.Comma, KeyCode.Period]);
+    const shiftNext = merge(
+      createShortcut([KeyCode.ShiftLeft, KeyCode.Backquote]),
+      createShortcut([KeyCode.ShiftRight, KeyCode.Backquote])
+    );
+    const altK = merge(
+      createShortcut([KeyCode.AltLeft, KeyCode.KeyK]),
+      createShortcut([KeyCode.AltRight, KeyCode.KeyK])
+    );
+    const altSpace = merge(
+      createShortcut([KeyCode.AltLeft, KeyCode.Space]),
+      createShortcut([KeyCode.AltRight, KeyCode.Space])
+    );
+
+    this.shortcuts$ = merge(
+      cmdJ,
+      ctrlL,
+      abc,
+      qwertz,
+      commaDot,
+      shiftNext,
+      altK,
+      altSpace
+    ).pipe(map((arr) => arr.map((a) => a.code).join("+")));
   }
 }
 
-export const createShortcut = (shortcut: string) => {
+export const createShortcut = (shortcut: KeyCode[]) => {
   const keyDown$ = fromEvent<KeyboardEvent>(document, "keydown");
   const keyUp$ = fromEvent<KeyboardEvent>(document, "keyup");
 
   const keyEvents = merge(keyDown$, keyUp$).pipe(
-    distinctUntilChanged((a, b) => a.key === b.key && a.type === b.type),
+    distinctUntilChanged((a, b) => a.code === b.code && a.type === b.type),
     share()
   );
 
-  const createKeyPressStream = (charCode) =>
-    keyEvents.pipe(filter((event) => event.key === charCode));
+  const createKeyPressStream = (charCode: KeyCode) =>
+    keyEvents.pipe(filter((event) => event.code === charCode.valueOf()));
 
-  return of(shortcut.split("+")).pipe(
-    switchMap((seq) =>
-      combineLatest([...seq.map((s) => createKeyPressStream(s))])
-    ),
+  return of(shortcut).pipe(
+    switchMap((seq) => combineLatest(seq.map((s) => createKeyPressStream(s)))),
     filter<KeyboardEvent[]>((arr) => arr.every((a) => a.type === "keydown"))
   );
 };
+
+export function sequence() {
+  return (source: Observable<KeyboardEvent[]>) => {
+    return source.pipe(
+      filter((arr) => {
+        const sorted = [...arr]
+          .sort((a, b) => (a.timeStamp < b.timeStamp ? -1 : 1))
+          .map((a) => a.code)
+          .join();
+        const sequence = arr.map((a) => a.code).join();
+        return sorted === sequence;
+      })
+    );
+  };
+}
